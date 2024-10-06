bd6d6d5407d6ea0c489b1ce9056af798397c6951
Eugene Oh Yun Zheng <eugeneoyz.12.03.n@gmail.com>
2024-10-06 15:27:31 +0800

    Implement hazard area

--- new-game-project/1_globals/level_manager.gd
+++ new-game-project/1_globals/level_manager.gd
@@ -6,14 +6,22 @@ const levels = {
 }
 
 var root : Node
+var curr_index : int = 1
 var curr : Node2D
 
 func _ready() -> void:
 	root = get_tree().root
 	curr = root.get_child(-1)
 
+func reset_level():
+	load_level(curr_index)
+
 func go_to_level(index):
+	load_level(index)
+
+func load_level(index):
 	root.remove_child(curr)
 	curr.queue_free()
 	curr = load(levels[index]).instantiate()
 	root.add_child(curr)
+	curr_index = index
